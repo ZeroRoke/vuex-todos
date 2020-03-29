5b5aceb67dce8d836b885bda50f9807fdbdfc924
overQ <1516494457@qq.com>
2020-03-29 10:02:37 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,49 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <a-input placeholder="请输入任务" class="inp"></a-input>
+    <a-button type='primary'>添加事项</a-button>
+    <a-list bordered :dataSource="list">
+      <a-list-item slot="renderItem" slot-scope="item">
+        <a-checkbox>{{item.info}}</a-checkbox>
+        <a slot="actions">删除</a>
+      </a-list-item>
+      <div slot="footer" class="footer">
+        <span>0条剩余</span>
+        <a-button-group>
+          <a-button type='primary'>全部</a-button>
+          <a-button type='primary'>未完成</a-button>
+          <a-button type='primary'>已完成</a-button>
+        </a-button-group>
+        <a-button type="link" block class="link">清除已完成</a-button>
+      </div>
+    </a-list>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data () {
+    return {
+      list: [
+        {
+          id: 0, info: '111111111', done: false
+        },
+        {
+          id: 1, info: '22222222', done: false
+        },
+        {
+          id: 2, info: '3333333', done: true
+        },
+        {
+          id: 3, info: '44444444', done: false
+        },
+        {
+          id: 4, info: '555555555', done: true
+        }
+      ]
+    }
   }
 }
 </script>
@@ -21,8 +53,20 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  text-align: left;
   color: #2c3e50;
   margin-top: 60px;
+  width: 800px;
+}
+input.inp{
+  width: 89%;
+}
+.footer{
+  display: flex;
+  justify-content: space-between;
+
+}
+button.link{
+  width: 74px;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,9 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
 import store from './store'
-
+import AntD from 'ant-design-vue'
+import 'ant-design-vue/dist/antd.css'
 Vue.config.productionTip = false
-
+Vue.use(AntD)
 new Vue({
   store,
   render: h => h(App)
